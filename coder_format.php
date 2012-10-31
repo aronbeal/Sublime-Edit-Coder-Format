daf7fab5f07197e2f66299f3567fe66f701dcb69
aronbeal <aron.beal+github@gmail.com>
2012-10-31 11:13:55 -0700

    Bugfixes, modified to work with sublime menu system instead of with a Project property.  Added ability to override key shortcuts, and eligible file suffixes

--- coder_format.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "./coder_format.inc";
-$buffer = file_get_contents('php://stdin');
-$stdout = fopen('php://stdout', 'w');
-fwrite($stdout, coder_format_string_all($buffer));
-fclose($stdin);
-fclose($stdout);
-exit(0);
-
