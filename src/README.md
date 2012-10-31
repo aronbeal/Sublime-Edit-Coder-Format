3dceeabefe478a40dfe69bf43e0e55903d2c535d
aronbeal <aron.beal+github@gmail.com>
2012-10-31 11:17:52 -0700

    Actual source not present in prior commits.

---
+++ src/README.md
@@ -0,0 +1,20 @@
+#CoderFormatOnSave
+This Sublime Text plugin is responsible for applying Drupal coder formatting to *.php and *.module (as of this version) files.  It does so leveraging the Coder module for drupal, part of which is bundled with this plugin.  For more information on the latter, see https://drupal.org/project/coder
+
+## Installation
+
+### Mac OSX (tested on 10.8)
+
+1. Clone git repo into your packages folder (mine is ~/Library/Application Support/Sublime Text 2/Packages):
+<code>
+  $ cd ~/Library/Application Support/Sublime Text 2/Packages
+
+  $ git clone https://github.com/aronbeal/Sublime-Edit-Coder-Format
+</code>
+2. Restart Sublime Text 2
+
+##Usage
+Only key binding so far is for OSX, which is set to ctrl+c to invoke coder formatting.  Also available under Tools->Coder Format->Coder Format, or as a quickwindow completion of "Coder Format".  Command is set to work only for .php, .install, and .module file suffixes; this can be changed under the preferences for the module (Preferences->Package Settings->Coder Format on OSX)
+
+##Other notes
+If you wish to use the version of coder present in another drupal instance, simply edit the "require" statement at the beginning of the coder_format.php file to point to the coder_format.inc file in your drupal instance.
---
+++ src/coderformat.py
@@ -0,0 +1,52 @@
+import sublime
+import sublime_plugin
+import re
+from os import chdir,curdir,path
+from subprocess import Popen, PIPE
+from UserString import MutableString
+
+
+class CoderFormat(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        current_view = sublime.active_window().active_view()
+        current_file = current_view.file_name()
+        if current_file == None:
+            print "Please save the file first"
+            return
+        settings = sublime.load_settings('Preferences.sublime-settings')
+        allowed_suffixes = settings.get('allowed_coder_format_suffixes', ['php'])
+        regex = []
+        regex.append("^.*\.(")
+        regex.append(("|".join(allowed_suffixes)))
+        regex.append(")$")
+        #print "Regex", ''.join(regex)
+        regex = re.compile(''.join(regex))
+        print "Coder Format request for file: ", current_file
+
+        is_interesting = regex.search(current_file)
+        #check to see if file suffix is matched.
+        if is_interesting is not None:
+            print "Coder formatting file..."
+            #buffer is non-empty
+            original_directory = curdir
+            chdir(sublime.packages_path())
+            chdir('CoderFormat')
+            try:
+                print "Applying coder format to file : ", myfile
+
+                edit = self.view.begin_edit('coder_format')
+                bufferRegion = sublime.Region(0, self.view.size())
+                bufferContent = self.view.substr(bufferRegion)
+                if not path.exists('./coder_format.php'):
+                    raise Exception("coder_format.php does not exist")
+                p = Popen(['/usr/bin/env', 'php', './coder_format.php'], stdin=PIPE, stdout=PIPE)
+
+                bufferContent = p.communicate(bufferContent)[0]
+                self.view.replace(edit, bufferRegion, bufferContent)
+                self.view.end_edit(edit)
+            finally:
+                chdir(original_directory)
+        else:
+            print "Not an eligible filetype.  Skipping."
+
